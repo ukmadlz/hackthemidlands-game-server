4cb74b35cee8ddc7a7a84c525cd42684f7e1a4cd
Mike Elsmore <mike@elsmore.me>
2017-09-23 16:36:35 +0100

    wildcard sockets

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, { origins: '*:*' });
 
 app.use(require('morgan')('combined'));
 app.use(require('cors')());
