c62b670462e8ca9622a196ad79d47d20d545d0ff
Mike Elsmore <mike@elsmore.me>
2017-09-23 14:19:09 +0100

    Timestamping and saving to in-memory cache

--- index.js
+++ index.js
@@ -1,18 +1,70 @@
 'use strict';
 
+//
+// App setup
+//
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 
 const io = require('socket.io')(server);
 
+app.use(require('morgan')('combined'));
+
+//
+// Timestamps
+//
+
+const Moment = require('moment');
+const moment = new Moment();
+
+//
+// In memory store
+//
+
+const NodeCache = require('node-cache');
+const gameCache = new NodeCache({ stdTTL: 0, checkperiod: 0 });
+
+//
+// Routes
+//
 app.get('/', (req, res) => {
-  res.send({});
+  res.send({
+    state: gameCache.get('game-state')
+      ? gameCache.get('game-state').state
+      : 'not-started'
+  });
 });
 
-io.on('connection', socket => {
-  socket.emit('news', { hello: 'world' });
+// Start a game
+app.get('/start', (req, res) => {
+  const gameStatus = {
+    state: 'start',
+    timestamp: moment.format()
+  };
+  gameCache.set('game-state', gameStatus, (err, success) => {
+    io.on('connection', socket => {
+      socket.emit('game', gameStatus);
+    });
+    res.send(gameStatus);
+  });
 });
+
+// End a game
+app.get('/end', (req, res) => {
+  const gameStatus = {
+    state: 'end',
+    timestamp: moment.format()
+  };
+  gameCache.set('game-state', gameStatus, (err, success) => {
+    io.on('connection', socket => {
+      socket.emit('game', gameStatus);
+    });
+    res.send(gameStatus);
+  });
+});
+
+// Server
 
 const port = process.env.PORT || 3000;
 
